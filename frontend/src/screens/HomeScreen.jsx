c8b3ab5f2429c525fa5f03c8f38ef6d676568525
Sumbul Vohra <sumbulmvohra@gmail.com>
2024-05-12 11:52:04 -0400

    redux toolkit and state management - loader and message components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,23 +1,22 @@
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
 
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async() => {
-      const {data} = await axios.get("/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
+      { isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          { error?.data?.message || error?.error }
+        </Message>
+      ) : (<>
         <h1>Latest Products</h1>
         <Row>
             {products.map((product) => (
@@ -25,7 +24,9 @@ const HomeScreen = () => {
                     <Product product={product} />
                 </Col>
             ))}    
-        </Row> 
+        </Row>
+      </>) }
+ 
     </>
   )
 }
